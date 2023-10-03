988d8d18d6bbd5e2f69bf11c4ce60ac74fa4c8bf
Charlotte Burman <charlotte.burman@ucl.ac.uk>
2023-10-03 13:59:09 +0100

    Trying to create conflict

--- first_script.R
+++ first_script.R
@@ -1,4 +1,4 @@
-df_test <- data.frame(var_1 = rnorm(100,0,1), var_2 = rnorm(100,1,2), var_3 = rnorm(100,4,5))
+df_test <- data.frame(var_1 = rnorm(100,0,1), var_2 = rnorm(100,1,2), var_3 = rnorm(100,4,5), var_4 = rnorm(100,5,6))
 
 # testing
 # testing12
