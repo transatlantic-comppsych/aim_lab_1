10a32a4266a6205929a3ad2c83326016f45027f7
IsobelRidler <i.ridler@ucl.ac.uk>
2023-10-04 12:52:10 +0100

    test list

--- first_script.R
+++ first_script.R
@@ -1,6 +1,9 @@
 df_test <- data.frame(var_1 = rnorm(100,0,1), var_2 = rnorm(100,1,2), var_3 = rnorm(100,4,5), var_4 = rnorm(100,5,6))
 
-df_test <- list(a = c(1,2,3), b = (4,5,6))
+
+df_test <- list(a = c(1,2,3), b = c(4,5,6))
+
+
 
 # testing
 # testing12
