b1ae8c9831349fd011a404d254aa1f2d88ec75aa
IsobelRidler <i.ridler@ucl.ac.uk>
2023-10-04 12:54:04 +0100

    combined objs

--- first_script.R
+++ first_script.R
@@ -1,7 +1,9 @@
 df_test <- data.frame(var_1 = rnorm(100,0,1), var_2 = rnorm(100,1,2), var_3 = rnorm(100,4,5), var_4 = rnorm(100,5,6))
 
 
-df_test <- list(a = c(1,2,3), b = c(4,5,6))
+df_test2 <- list(a = c(1,2,3), b = c(4,5,6))
+
+df_test3 <- list(df_test, df_test2)
 
 
 
