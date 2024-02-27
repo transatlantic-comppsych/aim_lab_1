7d61e3815a863d12d9ebcaedd2d96d54b5dedd16
Elena Bagdades <elena.bagdades.21@ucl.ac.uk>
2024-02-27 13:50:18 +0000

    Update Mini-SPIN_scoring.R

--- R code for clinical questionnaires/Mini-SPIN_scoring.R
+++ R code for clinical questionnaires/Mini-SPIN_scoring.R
@@ -5,9 +5,9 @@ library(dplyr)
 library(stringr)
 library(readxl)
 # Set your working directory to the folder in which all your CSV files are located
-setwd("/Users/Elena/Downloads/copy_data_exp_144636-v5")
-Mini_SPIN_data <- read.csv("data_exp_144636-v5_questionnaire-4h2a.csv")
-Mini_SPIN_data_clean <- LSAS_data[, c(31, 33, 35, 37)]
+setwd("/Users/Elena/Downloads/copy_data_exp_167771-v4")
+Mini_SPIN_data <- read.csv("SUP_PRF_p17_vid_bigPE_nar2_earlypred_chk_cbal_v4_mini_spin.csv")
+Mini_SPIN_data_clean <- Mini_SPIN_data[, c(32, 34, 36, 39)]
 rows_to_remove <- c(1, nrow(Mini_SPIN_data_clean))
 Mini_SPIN_data_clean <- Mini_SPIN_data_clean[-rows_to_remove, ]
 
@@ -16,7 +16,6 @@ non_id_cols <- setdiff(names(Mini_SPIN_data_clean), "Random_ID")
 
 # Convert non-ID variables to numeric
 Mini_SPIN_data_clean[, non_id_cols] <- lapply(Mini_SPIN_data_clean[, non_id_cols], as.numeric)
-
 Mini_SPIN_scores <-  numeric(length(Mini_SPIN_data_clean$Random_ID))
 Mini_SPIN_scores <- 0
 Questions <- c(1:3)
