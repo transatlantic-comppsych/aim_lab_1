afaa6fd87fe3a1a6584f2afccf206f13ba95f805
Elena Bagdades <elena.bagdades.21@ucl.ac.uk>
2024-05-13 14:05:08 +0100

    Update Clinical_srv.R

--- R code for clinical questionnaires/Clinical_srv.R
+++ R code for clinical questionnaires/Clinical_srv.R
@@ -5,12 +5,34 @@ library(readxl)
 # Set your working directory to the folder in which all your CSV files are located
 setwd("/Users/Elena/Downloads")
 clinical_srv_data <- read.csv("SUP_PRF_pilot_vid_big_PE_avatars_v9_clinical_srv.csv")
+names(clinical_srv_data)
 # Assuming your dataframe is named df
-selected_columns <- c(31, 33, 35, 37, 38, 40, 41, 42, 43, 44, 45, 47, 49, 50, 53, 55, 56, 57, 59, 60)  # Selecting columns 31 through 59 skipping every other column
+selected_columns <- c("Dropdown.object.19.Response",
+                      "Sex.object.25.Response",
+                      "Gender.object.24.Response",
+                      "Gender.object.24.Other",                                
+                      "mental_health_diagnosis.object.3.Response",  
+                      "other_mental_health_diagnoses.object.7.Depression",     
+                      "other_mental_health_diagnoses.object.7.Anxiety.disorder",
+                      "other_mental_health_diagnoses.object.7.ADHD",            
+                      "other_mental_health_diagnoses.object.7.Other",      
+                      "medication_status.object.5.Response",           
+                      "fluoxetine_prozac.object.10.Response",
+                      "dose_fluoxetine_prozac.object.11.Value",                
+                      "sertraline_zoloft.object.12.Response",                   
+                      "dose_sertraline_zoloft.object.13.Value",                
+                      "Ritalin.object.14.Response",
+                      "dose_Ritalin.object.15.Value",                           
+                      "other_medication.object.16.Value",                       
+                      "Ethnicity..object.28.Response",                      
+                      "Ethnicity..object.28.Other",                             
+                      "Random_ID")                            
+
+  
 clinical_srv_data_clean <- clinical_srv_data[, selected_columns]  # Subset your dataframe with selected columns
 
 # Generate new names for the columns
-new_names <- c("Age", "Sex", "Gender_same_as_birth", "Gender_other", "MH_diagnosis", "Depression", "Anxiety", "ADHD", "MH_other", "Medication_status", "fluoxetine_prozac", "flueoxtine_dosage", "sertraline_zoloft", "sertraline_dosage", "ritaln", "ritalin_dosage", "other_medications","Ethnicity", "Ethnicity_other", "Random_ID")
+new_names <- c("Age", "Sex", "Gender_same_as_birth", "Gender_other", "MH_diagnosis", "Depression", "Anxiety", "ADHD", "MH_other", "Medication_status", "fluoxetine_prozac", "flueoxtine_prozac_dosage", "sertraline_zoloft", "sertraline_zoloft_dosage", "ritaln", "ritalin_dosage", "other_medications", "Ethnicity", "Ethnicity_other", "Random_ID")
 
 # Rename the columns
 colnames(clinical_srv_data_clean)[1:20] <- new_names
