9e6727962f69690f6b36927a031f0f498b9c6269
elenabagdades <141220542+elenabagdades@users.noreply.github.com>
2025-05-28 10:36:50 +0100

    added pilot 21

--- Surprise_task_data_analysis/merge_all_datasets.R
+++ Surprise_task_data_analysis/merge_all_datasets.R
@@ -16,6 +16,13 @@ clean_data <- function(df) {
 }
 
 # Read and clean datasets
+data_pilot21 <- read.csv("/Users/elenabagdades/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Desktop/GitHub/aim_lab_1/Extracted_data/pilot_21_variables.csv") %>%
+  clean_data() %>%
+  rename(Depression_score = CESD_score) %>%
+  mutate(Dataset = "Pilot_21")
+data_pilot21[, c("Depression_score")] <- scale(data_pilot21[, c("Depression_score")])
+
+
 data_18_25_pro <- read.csv("final_df_18_25_Pro_with_stresslevels.csv") %>%
   clean_data() %>%
   rename(Depression_score = CESD_score) %>%
@@ -50,14 +57,16 @@ column_names_list <- list(
   colnames(data_18_25_pro),
   colnames(data_26_45_pro),
   colnames(data_14_18),
-  colnames(data_18_25_com)
+  colnames(data_18_25_com), 
+  colnames(data_pilot21)
+  
 )
 
 # Print column names for verification
 print(column_names_list)
 
 
-merged_data <- bind_rows(data_18_25_pro, data_26_45_pro, data_14_18, data_18_25_com)
+merged_data <- bind_rows(data_18_25_pro, data_26_45_pro, data_14_18, data_18_25_com, data_pilot21)
 
 # View the first few rows
 head(merged_data)
